55cb9fc741c2d7b0476d2aec3d67818cd50cfba1
Mohammad Kavousi <mohamad.kav@gmail.com>
2017-05-21 00:22:17 +0430

    init ARCH

---
+++ SUCHME/core/src/main/java/ir/suchme/core/CoreApplication.java
@@ -0,0 +1,14 @@
+package ir.suchme.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mohammad on 5/20/17.
+ */
+@SpringBootApplication
+public class CoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class,args);
+    }
+}
