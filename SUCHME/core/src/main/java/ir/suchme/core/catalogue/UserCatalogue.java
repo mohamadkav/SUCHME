d37d1aa382ea38ff0ada6862749d1caa7881b5a9
Mohammad Kavousi <mohamad.kav@gmail.com>
2017-06-08 18:07:01 +0430

    Please don't let me commit this :(

---
+++ SUCHME/core/src/main/java/ir/suchme/core/catalogue/UserCatalogue.java
@@ -0,0 +1,61 @@
+package ir.suchme.core.catalogue;
+
+import ir.suchme.common.dto.user.RequestAuthenticateDTO;
+import ir.suchme.core.domain.entity.User;
+import ir.suchme.core.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by mohammad on 6/8/17.
+ */
+@Component
+public class UserCatalogue {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserCatalogue(AuthenticationManager authenticationManager,UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository=userRepository;
+    }
+
+    public void loginSpring(HttpServletRequest httpServletRequest, RequestAuthenticateDTO request){
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(request.getUserName(), request.getPassword());
+
+        // Authenticate the user
+        Authentication authentication = authenticationManager.authenticate(authRequest);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        // Create a new session and add the security context.
+        HttpSession session = httpServletRequest.getSession(true);
+        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+    }
+
+    public User findUser(String userName){
+        return userRepository.findByUserNameAndDeletedIsFalse(userName);
+    }
+    public Iterable<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public void addUser(String userName,String password){
+        User user=new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        user.setCreated(new Date());
+        userRepository.save(user);
+    }
+}
--- SUCHME/core/src/main/java/ir/suchme/core/service/UserService.java
+++ SUCHME/core/src/main/java/ir/suchme/core/service/UserService.java
@@ -3,6 +3,7 @@ package ir.suchme.core.service;
 import ir.suchme.common.dto.base.BaseResponseDTO;
 import ir.suchme.common.dto.user.RequestAuthenticateDTO;
 import ir.suchme.common.dto.user.ResponseUserDTO;
+import ir.suchme.core.catalogue.UserCatalogue;
 import ir.suchme.core.domain.entity.User;
 import ir.suchme.core.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,51 +26,34 @@ import java.util.UUID;
 @Service
 public class UserService {
 
-    private final AuthenticationManager authenticationManager;
-
-    private final UserRepository userRepository;
-
+    private final UserCatalogue userCatalogue;
     @Autowired
-    public UserService(AuthenticationManager authenticationManager, UserRepository userRepository) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
+    public UserService(UserCatalogue userCatalogue) {
+        this.userCatalogue=userCatalogue;
     }
 
     public ResponseUserDTO auth(HttpServletRequest httpServletRequest, RequestAuthenticateDTO request) throws Exception {
-        User user=userRepository.findByUserNameAndDeletedIsFalse(request.getUserName());
+        User user=userCatalogue.findUser(request.getUserName());
         ResponseUserDTO response;
         if(user==null)
             response=new ResponseUserDTO("User not found",null,"301");
         else {
             response = new ResponseUserDTO(user.getId().toString(), "0");
-            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(request.getUserName(), request.getPassword());
-
-            // Authenticate the user
-            Authentication authentication = authenticationManager.authenticate(authRequest);
-            SecurityContext securityContext = SecurityContextHolder.getContext();
-            securityContext.setAuthentication(authentication);
-
-            // Create a new session and add the security context.
-            HttpSession session = httpServletRequest.getSession(true);
-            session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+            userCatalogue.loginSpring(httpServletRequest,request);
         }
         return response;
     }
 
     public BaseResponseDTO create(RequestAuthenticateDTO request){
-        if(userRepository.findByUserNameAndDeletedIsFalse(request.getUserName())!=null)
+        if(userCatalogue.findUser(request.getUserName())!=null)
             return new BaseResponseDTO("User already exists","400",null);
-        User user=new User();
-        user.setUserName(request.getUserName());
-        user.setPassword(request.getPassword());
-        user.setCreated(new Date());
-        userRepository.save(user);
+        userCatalogue.addUser(request.getUserName(),request.getPassword());
         return new BaseResponseDTO(null,"0",null);
     }
 
     public ArrayList<ResponseUserDTO> list(){
         ArrayList<ResponseUserDTO> dtos = new ArrayList<>();
-        for (User u: userRepository.findAll())
+        for (User u: userCatalogue.findAll())
         {
             ResponseUserDTO dto = new ResponseUserDTO();
             dto.setUserId(u.getId().toString());
